3cfd14e587efd08f207930ef4e6a52738284b26d
Андрей Немкович <nemkovich_as@nectech.pro>
2025-08-25 21:29:52 +0300

    output data in server.py is like in the requirements

--- server.py
+++ server.py
@@ -17,17 +17,24 @@ def sent_detector():
     '''
     text_to_analyze = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyze)
-    return f"The given text has been identified as {label} with a score of {score}."
+    # Create str with emotions for answer without dominant emotion
+    items = []
+    for k, v in response.items():
+        if k != 'dominant_emotion':
+            items.append(f"'{k}': {v}")
+    emotions_str = ", ".join(items[:-1]) + " and " + items[-1]
+    # Find dominant emotion
+    dominant = response['dominant_emotion']
+    return f"For the given statement, the system response is {emotions_str}. The dominant emotion is {dominant}."
 
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
         page over the Flask channel
     '''
-    #TODO
     return render_template("index.html")
 
 if __name__ == "__main__":
     ''' This functions executes the flask app and deploys it on localhost:5000
-    '''#TODO
+    '''
     app.run(host="0.0.0.0", port=5000)
